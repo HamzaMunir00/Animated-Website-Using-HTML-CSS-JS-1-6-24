5622db51aa7d96020e2a0ce7c7a6e7dab6bb39db
Hamza Munir <hamzamunir883@gmail.com>
2024-06-10 11:23:38 +0500

    Initial commit

---
+++ lazarev.js
@@ -0,0 +1,69 @@
+function navAnimation() {
+  const nav = document.querySelector("nav");
+  const navBottom = document.querySelectorAll("#nav-bottom");
+
+  nav.addEventListener("mouseenter", function () {
+    let tl = gsap.timeline();
+
+    tl.to("#nav-bottom", {
+      height: "21vh",
+    });
+    tl.to(".nav-part2 h5", {
+      display: "block",
+    });
+    tl.to(".nav-part2 h5 span", {
+      y: 0,
+      // duration: 0.3,
+      stagger: {
+        amount: 0.5,
+      },
+    });
+  });
+  nav.addEventListener("mouseleave", function () {
+    let tl = gsap.timeline();
+    tl.to(".nav-part2 h5 span", {
+      // transform: `translateY(25px)`,
+      y: 25,
+      stagger: {
+        amount: 0.2,
+      },
+    });
+    tl.to(".nav-part2 h5", {
+      display: "none",
+      duration: 0.1,
+    });
+    tl.to("#nav-bottom", {
+      height: 0,
+      duration: 0.2,
+    });
+  });
+}
+// navAnimation();
+// /-------------
+
+function page2Animation() {
+  let rightElems = document.querySelectorAll(".right-elem");
+
+  rightElems.forEach(function (elem) {
+    elem.addEventListener("mouseenter", function () {
+      gsap.to(elem.childNodes[3], {
+        opacity: 1,
+        scale: 1,
+      });
+    });
+    elem.addEventListener("mouseleave", function () {
+      gsap.to(elem.childNodes[3], {
+        opacity: 0,
+        scale: 0,
+      });
+    });
+    elem.addEventListener("mousemove", function (dets) {
+      gsap.to(elem.childNodes[3], {
+        x: dets.x - elem.getBoundingClientRect().x - 90,
+        y: dets.y - elem.getBoundingClientRect().y - 200,
+      });
+    });
+  });
+}
+page2Animation();
+// --------------
